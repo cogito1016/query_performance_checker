d852c83f723d6d406a0f58bc328a0c6f2cde3a39
김재형 <cogito1016@naver.com>
2023-03-26 18:27:51 +0900

    feat: temporary integration

--- src/query-performance-checker/index.ts
+++ src/query-performance-checker/index.ts
@@ -1,9 +1,23 @@
-import connection from "./connection-setter";
+import { Connection, createConnection } from "mysql";
 
-const queryPerformanceChecker = () => {
-  const conn = connection();
+const queryPerformanceChecker = async () => {
+  const connection: Connection = createConnection({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || "3306", 10),
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
 
-  return conn;
+  connection.query("", (error, rows, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(rows);
+    }
+
+    connection.end();
+  });
 };
 
 export default queryPerformanceChecker;
